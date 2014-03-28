223989b7d5eb2acd12c2f89636422cc906a51a14
CC.tgz <qiu6288@gmail.com>
2014-03-28 13:11:07 +0800

    说明

---
+++ QR_QX/default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Util;
+using ThoughtWorks.QRCode.Codec.Data;
+namespace QR_QX
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request["url"].Length<20)
+            {
+                Response.Write("生成失败！");
+                return;
+            }
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.Q;
+            qrCodeEncoder.QRCodeScale = 4;
+            qrCodeEncoder.QRCodeVersion = 9;
+            String url = Request["url"].ToString();
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.Drawing.Image img = qrCodeEncoder.Encode(url);
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            Response.ClearContent();
+            Response.ContentType = "image/gif";
+            Response.BinaryWrite(ms.ToArray());
+            Response.End();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 QRcreate
 ========
+>使用：ThoughtWorks.QRCode.dll类库制作
 
-访问主页附上？url=xxx，为XXX生成二维码图片
+>访问主页附上？url=xxx，为XXX生成二维码图片
+
+
+>比如
+
+>		http://192.168.0.84/QR/default.aspx?url=http://192.168.0.84/
+
+>生成的二维码信息为：http://192.168.0.84/
